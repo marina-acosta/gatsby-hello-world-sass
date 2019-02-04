5d16ad2849718e95b5b2b9e253e28ffc94b354c5
Marina Alicia Acosta Pedemonte <marina.acosta@fing.edu.uy>
2019-02-04 09:16:06 -0300

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Common pieces of data shared across the site
+  siteMetadata: {
+    title: `Gatsby`,
+    description: `Blazing fast modern site generator for React`
+  },
+  // Plugins: node packages that implement Gatsby functionalities
+  plugins: [`gatsby-plugin-sass`]
+};
